c9de634a12d8ad1e6c440047c9dbf7b7dfa6ec32
Kirill Lysov <cyrillicns@gmail.com>
2024-05-26 18:11:32 +0500

    Complete Game Board

--- game.py
+++ game.py
@@ -1,17 +1,56 @@
 from inspect import getsource
-from game_parts import Board
+from game_parts import Board, FieldIndexError, CellOccupiedError
 
 
 def main():
-    game_1 = Board()
-    game_1.make_move(0, 1, 'x')
-    print('Ход сделан!')
-    game_1.display()
-    print(game_1)
+    game = Board()
+    current_player = 'X'
+    running_flag = True
+    game.display()
+
+    while running_flag:
+        print(f'Ход делает игрок - {current_player}')
+        while True:
+            try:
+                row = int(input('Введите номер номер ряда: '))
+                col = int(input('Введите номер столбца: '))
+                if (row > 2 or row < 0) or (col > 2 or col < 0):
+                    raise FieldIndexError
+                if isinstance(row, int) == False or isinstance(col, int) == False:
+                    raise ValueError
+                if game.board[row][col] != ' ':
+                    raise CellOccupiedError
+            except CellOccupiedError:
+                print('Ячейка занята - выберите другую')
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Пожалуйста, введите значения для строки и столбца заново.')
+                continue
+            except FieldIndexError:
+                print(f'Ошибка: {FieldIndexError}')
+                print(f'Значение должно быть меньше '
+                      f'{game.field_size} и больше 0 ')
+                print('Введите новое значение')
+                continue
+            except Exception:
+                print(f'Произошла непредвиденная ошибка - {Exception}')
+                continue
+            else:
+                break
+
+        game.make_move(row, col, current_player)
+        print('Ход сделан!')
+        print()
+        game.display()
+        if game.check_win(current_player):
+            print(f'Победил игрок - {current_player}')
+            running_flag = False
+        elif game.is_board_full():
+            print('Ничья!')
+            running_flag = False
+        current_player = 'O' if current_player == 'X' else 'X'
+        print()
 
 
 if __name__ == __name__:
     main()
-
-game_1 = Board()
-print(getsource(Board))
--- game_parts/parts.py
+++ game_parts/parts.py
@@ -1,9 +1,11 @@
 class Board:
-    
+    """Класс описывающий игровое поле"""
+
     field_size = 3
 
     def __init__(self):
-        self.board = [[' ' for _ in range(self.field_size)] for _ in range(self.field_size)]
+        self.board = [[' ' for _ in range(self.field_size)]
+                      for _ in range(self.field_size)]
 
     def make_move(self, row, col, player):
         self.board[row][col] = player
@@ -13,6 +15,23 @@ class Board:
             print('|'.join(row))
             print('-----')
 
-    def __str__(self):
-        return (f'Объект игрового поля размером: '
-                f'{self.field_size} x {self.field_size}')
+    def is_board_full(self):
+        for i in range(self.field_size):
+            for j in range(self.field_size):
+                if self.board[i][j] == ' ':
+                    return False
+        return True
+
+    def check_win(self, player):
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+        if ((self.board[0][0] == self.board[1][1] == self.board[2][2] == player) or
+                self.board[2][0] == self.board[1][1] == self.board[0][2] == player):
+            return True
+        return False
+
+        def __str__(self):
+            return (f'Объект игрового поля размером: '
+                    f'{self.field_size} x {self.field_size}')
